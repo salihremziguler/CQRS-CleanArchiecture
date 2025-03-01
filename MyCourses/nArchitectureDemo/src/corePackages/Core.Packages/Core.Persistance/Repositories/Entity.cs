58c42bf1288423f224cca8077791277251922601
salihremziguler <salihremziguler@gmail.com>
2025-03-02 01:46:59 +0300

    project started

---
+++ MyCourses/nArchitectureDemo/src/corePackages/Core.Packages/Core.Persistance/Repositories/Entity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistance.Repositories
+{
+    public class Entity<T>
+    {
+        public T Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime? UpdatedDate { get; set; }
+
+        public DateTime? DeletedDate { get; set; }
+
+        public Entity()
+        {
+            Id = default;
+
+        }
+
+        public Entity(T id)
+        {
+            Id = id;
+            
+        }
+
+    }
+}
---
+++ MyCourses/nArchitectureDemo/src/rentACar/RentACar/WebApi/Controllers/BrandsController.cs
@@ -0,0 +1,23 @@
+﻿using Application.Features.Brands.Commands.Create;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : BaseController
+    {
+
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateBrandCommand createBrandCommand)
+        {
+          CreatedBrandResponse createdBrandResponse=  await Mediator.Send(createBrandCommand);
+          return Ok(createdBrandResponse);
+            
+        }
+
+
+    }
+}
